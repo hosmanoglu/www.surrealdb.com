c3bacb3368dfb8f98a57d6ee6dec1fee254ca15a
hosmanoglu <70819624+hosmanoglu@users.noreply.github.com>
2023-11-25 15:16:02 +0300

    Update basic.js
    i changed with https://www.npmjs.com/package/surrealdb.node

--- app/snippets/docs/integration/libraries/nodejs/basic.js
+++ app/snippets/docs/integration/libraries/nodejs/basic.js
@@ -1,56 +1,57 @@
-const { default: Surreal } = require('surrealdb.node');
+// import as ES module or common JS
+import { Surreal } from 'surrealdb.node';
+const { Surreal } = require('surrealdb.node');
 
 const db = new Surreal();
 
 async function main() {
-	try {
+    try {
+        // use in-memory database
+        await db.connect('memory');
+        // connect to database server
+        await db.connect('ws://127.0.0.1:8000');
+        // use rocksdb file
+        await db.connect(`rocksdb://${process.cwd()}/test.db`);
 
-		// Use any of these 3 connect methods to connect to the database
-		// 1.Connect to the database
-		await db.connect('http://127.0.0.1:8000/rpc');
-		// 2. Connect to database server
-		await db.connect('ws://127.0.0.1:8000');
-		// 3. Connect via rocksdb file
-		await db.connect(`rocksdb://${process.cwd()}/test.db`);
+        // Select a specific namespace / database
+        await db.use({ ns: "test", db: "test" });
 
-		// Signin as a namespace, database, or root user
-		await db.signin({
-			username: 'root',
-			password: 'root',
-		});
+        // Create a new person with a random id
+        let created = await db.create("person", {
+            title: "Founder & CEO",
+            name: {
+                first: "Tobie",
+                last: "Morgan Hitchcock",
+            },
+            marketing: true,
+            identifier: Math.random().toString(36).substr(2, 10),
+        });
 
-		// Select a specific namespace / database
-		await db.use({ namespace: 'test', database: 'test' });
+        // Update a person record with a specific id
+        let updated = await db.merge("person:jaime", {
+            marketing: true
+        });
 
-		// Create a new person with a random id
-		let created = await db.create('person', {
-			title: 'Founder & CEO',
-			name: {
-				first: 'Tobie',
-				last: 'Morgan Hitchcock',
-			},
-			marketing: true,
-			identifier: Math.random().toString(36).slice(2, 12),
-		});
+        // Select all people records
+        let people = await db.select("person");
 
-		// Update a person record with a specific id
-		let updated = await db.merge('person:jaime', {
-			marketing: true,
-		});
+        // Perform a custom advanced query
+        let groups = await db.query(
+            "SELECT marketing, count() FROM type::table($table) GROUP BY marketing",
+            {
+                table: "person",
+            },
+        );
 
-		// Select all people records
-		let people = await db.select('person');
+        // Delete all people upto but not including Jaime
+        let deleted = await db.delete("person:..jaime");
 
-		// Perform a custom advanced query
-		let groups = await db.query(
-			'SELECT marketing, count() FROM type::table($tb) GROUP BY marketing',
-			{
-				tb: 'person',
-			}
-		);
-	} catch (e) {
-		console.error('ERROR', e);
-	}
+        // Delete all people
+        await db.delete("person");
+
+    } catch (e) {
+        console.error('ERROR', e);
+    }
 }
 
 main();
